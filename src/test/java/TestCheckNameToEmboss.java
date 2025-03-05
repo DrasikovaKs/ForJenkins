cdf05907109247f878774648ad2875a56f2d5c72
Kseniya <ksenia.drasikova@mail.ru>
2025-03-05 15:32:51 +0300

    junit5 update

--- src/test/java/TestCheckNameToEmboss.java
+++ src/test/java/TestCheckNameToEmboss.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Step;
 import org.example.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -7,8 +8,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestCheckNameToEmboss {
 
+
     @DisplayName("Параметризированная проверка: можно ли напечатать строку на карте")
-    @ParameterizedTest
+    @ParameterizedTest(name = "Проверка имени: {0}, {1}")
     @CsvSource({
             "'Антошка Картошка', true",
             "'Ан', false",
@@ -17,8 +19,9 @@ public class TestCheckNameToEmboss {
             "'АнтошкаКартошка', false",
             "'Антошка  Картошка', false"
     })
+    @Step("Проверка")
     void check(String name, boolean expected) {
         Assertions.assertEquals(expected, new Account(name).checkNameToEmboss());
     }
-
 }
+
